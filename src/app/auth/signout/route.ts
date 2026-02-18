7e325d73896e2908941ce07b89db848c3ba4f198
Самвел Цаканян <Samvel@ugra.tvr>
2026-02-18 10:17:04 +0500

    Fix logout redirect logic

--- src/app/auth/signout/route.ts
+++ src/app/auth/signout/route.ts
@@ -3,13 +3,17 @@ import { cookies } from 'next/headers'
 import { NextResponse } from 'next/server'
 
 export async function POST(request: Request) {
+    // Определяем URL для редиректа, используя заголовки, чтобы избежать 0.0.0.0
     const requestUrl = new URL(request.url)
+    const host = request.headers.get('host') ?? requestUrl.host
+    const protocol = request.headers.get('x-forwarded-proto') ?? requestUrl.protocol.replace(':', '')
+
+    // Выходим из аккаунта
     const cookieStore = cookies()
     const supabase = createRouteHandlerClient({ cookies: () => cookieStore })
-
     await supabase.auth.signOut()
 
-    return NextResponse.redirect(`${requestUrl.origin}/login`, {
-        status: 301,
+    return NextResponse.redirect(`${protocol}://${host}/login`, {
+        status: 302,
     })
 }
